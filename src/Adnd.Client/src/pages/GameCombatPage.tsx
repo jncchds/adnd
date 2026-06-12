12efab65a07f92d3c173d13eaee8e9f98b61a3d4
Kirill Chekanov <checkatillo@gmail.com>
2026-06-12 12:34:35 +0200

    feat: add GameCombatPage as dedicated combat page

---
+++ src/Adnd.Client/src/pages/GameCombatPage.tsx
@@ -0,0 +1,8 @@
+import { useParams } from 'react-router-dom';
+import CombatTab from '../components/combat/CombatTab';
+
+export default function GameCombatPage() {
+  const { id } = useParams<{ id: string }>();
+  if (!id) return null;
+  return <CombatTab gameId={id} />;
+}
